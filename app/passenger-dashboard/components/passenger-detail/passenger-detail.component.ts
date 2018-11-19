164667b1ac7d7735f42f5869d804def0c6c49594
Paul Rieutord <prieutord@kunder.cl>
2018-11-19 09:48:01 -0300

    tutorial done

---
+++ app/passenger-dashboard/components/passenger-detail/passenger-detail.component.ts
@@ -0,0 +1,82 @@
+import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { Passenger } from '../../models/passenger.interface';
+
+@Component({
+    selector: 'passenger-detail',
+    styleUrls: ['passenger-detail.component.scss'],
+    template: `
+        <div>
+            <span class="status" [class.checked-in]="detail.checkedIn"></span>
+            <div *ngIf="editing">
+                <input
+                    type="text"
+                    [value]="detail.fullname"
+                    (input)="onNameChange(name.value)"
+                    #name
+                >
+            </div>
+            <div *ngIf="!editing">
+                {{ detail.fullname }}
+            </div>
+            <div class="date">
+                Check in date:
+                {{ detail.checkInDate ? (detail.checkInDate | date: 'yMMMMd') : 'Not checked in!' }}
+            </div>
+            <div class="children">
+                Children: {{ detail.children?.length || 0 }}
+            </div>
+            <button (click)="toggleEdit()">
+                {{ editing ? 'Done' : 'Editing' }}
+            </button>
+            <button (click)="onRemove()">
+                Remove
+            </button>
+            <button (click)="goToPassenger()">
+                View
+            </button>
+        </div>
+    `
+})
+
+export class PassengerDetailComponent implements OnChanges{
+    @Input()
+    detail: Passenger;
+
+    @Output()
+    edit: EventEmitter<any> = new EventEmitter();
+
+    @Output()
+    remove: EventEmitter<any> = new EventEmitter();
+
+    @Output()
+    view: EventEmitter<any> = new EventEmitter();
+
+    editing: boolean = false;
+
+    constructor() {}
+
+    ngOnChanges(changes) {
+        if (changes.detail) {
+            this.detail = Object.assign({}, changes.detail.currentValue)
+        }
+    }
+
+    onNameChange(value: string) {
+        this.detail.fullname = value;
+    }
+
+    goToPassenger() {
+        this.view.emit(this.detail);
+    }
+
+    toggleEdit() {
+        if (this.editing) {
+            this.edit.emit(this.detail);
+        }
+        this.editing = !this.editing;
+    }
+
+    onRemove() {
+        this.remove.emit(this.detail);
+    }
+}
